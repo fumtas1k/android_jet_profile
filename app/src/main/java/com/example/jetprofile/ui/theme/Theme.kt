f03b82629bd6fe169171514408bc020d51ca9939
fumtas1k <fumta01a7@gmail.com>
2022-11-19 13:29:47 +0900

    [change]フォーマット等の設定変更

--- app/src/main/java/com/example/jetprofile/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetprofile/ui/theme/Theme.kt
@@ -29,16 +29,16 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun JetProfileTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+  val colors = if (darkTheme) {
+    DarkColorPalette
+  } else {
+    LightColorPalette
+  }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+  MaterialTheme(
+      colors = colors,
+      typography = Typography,
+      shapes = Shapes,
+      content = content
+  )
 }
